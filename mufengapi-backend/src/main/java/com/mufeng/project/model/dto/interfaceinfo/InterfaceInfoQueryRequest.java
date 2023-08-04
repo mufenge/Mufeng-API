36d63ee7678e696553eec85336bb9a85f6baf66a
fengqing233 <2216391020@qq.com>
2023-08-04 16:46:05 +0800

    update base

---
+++ mufengapi-backend/src/main/java/com/mufeng/project/model/dto/interfaceinfo/InterfaceInfoQueryRequest.java
@@ -0,0 +1,61 @@
+package com.mufeng.project.model.dto.interfaceinfo;
+
+import com.mufeng.project.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 查询请求
+ *
+ * @author yupi
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class InterfaceInfoQueryRequest extends PageRequest implements Serializable {
+
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 接口地址
+     */
+    private String url;
+
+    /**
+     * 请求头
+     */
+    private String requestHeader;
+
+    /**
+     * 响应头
+     */
+    private String responseHeader;
+
+    /**
+     * 创建人
+     */
+    private String userId;
+
+    /**
+     * 接口状态（0 - 关闭， 1 - 开启））
+     */
+    private Integer status;
+
+    /**
+     * 请求类型
+     */
+    private String method;
+
+
+}
